388b737b47f55585104745ff351ab12a8a2c7020
Vedpatil09 <165073495+Vedpatil09@users.noreply.github.com>
2025-10-20 00:55:14 +0530

    Time: 1 ms (99.59%) | Memory: 45.5 MB (45.99%) - LeetSync

--- Array/2032-largest-odd-number-in-string/largest-odd-number-in-string.java
+++ Array/2032-largest-odd-number-in-string/largest-odd-number-in-string.java
@@ -1,18 +1,31 @@
+// class Solution {
+//     public String largestOddNumber(String num) {
+//         int i=num.length()-1;
+//         boolean flag=false;
+//         StringBuilder ans=new StringBuilder();
+//         for(;i>=0;i--){
+//             if((num.charAt(i)-'0')%2!=0){
+//                 flag=true;
+//                 break;
+//         }
+        
+//     }if(flag==true){
+//         for(int j=0;j<=i;j++){
+//             ans.append(num.charAt(j));
+//         }
+//         }        return ans.toString();
+//     }
+// }
 class Solution {
     public String largestOddNumber(String num) {
-        int i=num.length()-1;
-        boolean flag=false;
-        StringBuilder ans=new StringBuilder();
-        for(;i>=0;i--){
-            if((num.charAt(i)-'0')%2!=0){
-                flag=true;
-                break;
+        // Traverse from the end to find the last odd digit
+        for (int i = num.length() - 1; i >= 0; i--) {
+            int digit = num.charAt(i) - '0';
+            if (digit % 2 != 0) {
+                // return substring up to this point (inclusive)
+                return num.substring(0, i + 1);
+            }
         }
-        
-    }if(flag==true){
-        for(int j=0;j<=i;j++){
-            ans.append(num.charAt(j));
-        }
-        }        return ans.toString();
+        return ""; // no odd digit found
     }
 }
