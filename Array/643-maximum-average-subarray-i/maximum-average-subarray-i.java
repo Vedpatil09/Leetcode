4d7ea1157a8930c45d5506dade638dd71b4b1067
Ved Patil <165073495+Vedpatil09@users.noreply.github.com>
2026-04-12 01:06:01 +0530

    Time: 5 ms (37.98%) | Memory: 69.6 MB (68.37%) - LeetSync

--- Array/643-maximum-average-subarray-i/maximum-average-subarray-i.java
+++ Array/643-maximum-average-subarray-i/maximum-average-subarray-i.java
@@ -1,22 +1,43 @@
+// class Solution {
+//     public double findMaxAverage(int[] nums, int k) {
+//         double sum = 0;
+//         double avg = Integer.MIN_VALUE;
+//         double maxAvg = Integer.MIN_VALUE;
+//         int j = 0;
+//         for (int i = 0; i < nums.length; i++) {
+//             if (i <= k - 2) {
+//                 sum += (double) nums[i];
+//             } else {
+//                 sum += (double) nums[i];
+//                 avg = sum / k;
+//                 maxAvg = Math.max(maxAvg, avg);
+//                 sum -= (double) nums[j];
+//                 j++;
+//             }
+
+//         }
+
+//         return maxAvg;
+//     }
+// }
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
         double sum = 0;
-        double avg = Integer.MIN_VALUE;
-        double maxAvg = Integer.MIN_VALUE;
-        int j = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (i <= k - 2) {
-                sum += (double) nums[i];
-            } else {
-                sum += (double) nums[i];
-                avg = sum / k;
-                maxAvg = Math.max(maxAvg, avg);
-                sum -= (double) nums[j];
-                j++;
-            }
 
+        // Step 1: First window
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
         }
 
-        return maxAvg;
+        double maxSum = sum;
+
+        // Step 2: Sliding window
+        for (int i = k; i < nums.length; i++) {
+            sum += nums[i];      // add next element
+            sum -= nums[i - k];  // remove previous element
+            maxSum = Math.max(maxSum, sum);
+        }
+
+        return maxSum / k;
     }
 }
